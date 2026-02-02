30a1cf64d26e8259e628c30f6fc8b83e7fecd5eb
flexa01 <cetinogluhasan92@gmail.com>
2026-02-02 22:42:06 +0300

    Update page.tsx

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,1 +1,9 @@
+export default function PrivacyPage() {
+  return (
+    <main style={{ padding: 60, color: "white" }}>
+      <h1>Privacy Policy</h1>
+      <p>This is the privacy policy.</p>
+    </main>
+  );
+}
 
