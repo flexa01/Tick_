5ade000ea887b6073d14d02166e1d63ca46e3f5f
flexa01 <cetinogluhasan92@gmail.com>
2026-02-02 22:42:38 +0300

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,1 +1,9 @@
+export default function TermsPage() {
+  return (
+    <main style={{ padding: 60, color: "white" }}>
+      <h1>Terms of Use</h1>
+      <p>These are the terms of use.</p>
+    </main>
+  );
+}
 
